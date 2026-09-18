4c2993707d132a47086e30e21233df3f5b3e6d3e
Dusenswf <wkdduswn7338@naver.com>
2026-09-18 11:19:01 +0900

    first commit

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    void testMethod() {
+        Game game = new Game();
+        Assertions.assertEquals(10, 10);
+    }
+}
