ff5af4a5a2dc289be2b5dadcad36bff0305cb3f4
Dusenswf <wkdduswn7338@naver.com>
2026-09-18 11:29:08 +0900

    [feature] 34p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,10 +1,18 @@
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
     @Test
-    void testMethod() {
+    void createGame() {
         Game game = new Game();
-        Assertions.assertEquals(10, 10);
+        assertNotNull(game);
     }
+
+    @Test void throwExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Game game = new Game();
+            game.guess(null);
+        });
+    }
+
 }
