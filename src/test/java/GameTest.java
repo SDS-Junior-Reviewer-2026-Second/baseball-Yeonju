da48b830fa7e97e9c75eda5a69c2ec31b7b4e1e9
Dusenswf <wkdduswn7338@naver.com>
2026-09-18 11:31:05 +0900

    [refactoring] 36p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,16 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     void createGame() {
-        Game game = new Game();
         assertNotNull(game);
     }
 
     @Test void throwExceptionWhenInputIsNull() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Game game = new Game();
             game.guess(null);
         });
     }
